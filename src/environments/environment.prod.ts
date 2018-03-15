e383cc0fee12f62d4998d2f639451acd994089b6
mbuckles <bucklesm21@gmail.com>
2018-03-14 20:02:45 -0400

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: true,
    // current api
    WEATHER_APIKEY: '&appid=691c1e153351f780c9a6e0268402a27b',
-   WEATHER_CITY: 'http://api.openweathermap.org/data/2.5/weather?q=',
-   WEATHER_ZIP: 'http://api.openweathermap.org/data/2.5/weather?zip=',
-   WEATHER_POS: 'http://api.openweathermap.org/data/2.5/weather?',
+   WEATHER_CITY: 'https://api.openweathermap.org/data/2.5/weather?q=',
+   WEATHER_ZIP: 'https://api.openweathermap.org/data/2.5/weather?zip=',
+   WEATHER_POS: 'https://api.openweathermap.org/data/2.5/weather?',
   //  LOCATION_URL: 'http://ip-api.com/json',
    LOCATION_URL: 'https://ipinfo.io/json',
-   CURRENT_URL: 'http://api.openweathermap.org/data/2.5/weather?lat=',
+   CURRENT_URL: 'https://api.openweathermap.org/data/2.5/weather?lat=',
 
    // forecast api
-   FORECAST_CITY: 'http://api.openweathermap.org/data/2.5/forecast?q=',
-   FORECAST_ZIP: 'http://api.openweathermap.org/data/2.5/forecast?zip='
+   FORECAST_CITY: 'https://api.openweathermap.org/data/2.5/forecast?q=',
+   FORECAST_ZIP: 'https://api.openweathermap.org/data/2.5/forecast?zip='
 
  };
   //  http://api.openweathermap.org/data/2.5/forecast?q=atlanta&units=imperial&appid=691c1e153351f780c9a6e0268402a27b;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,15 @@ export const environment = {
   production: false,
    // current api
    WEATHER_APIKEY: '&appid=691c1e153351f780c9a6e0268402a27b',
-   WEATHER_CITY: 'http://api.openweathermap.org/data/2.5/weather?q=',
-   WEATHER_ZIP: 'http://api.openweathermap.org/data/2.5/weather?zip=',
+   WEATHER_CITY: 'https://api.openweathermap.org/data/2.5/weather?q=',
+   WEATHER_ZIP: 'https://api.openweathermap.org/data/2.5/weather?zip=',
   //  LOCATION_URL: 'http://ip-api.com/json',
    LOCATION_URL: 'https://ipinfo.io/json',
-   CURRENT_URL: 'http://api.openweathermap.org/data/2.5/weather?lat=',
+   CURRENT_URL: 'https://api.openweathermap.org/data/2.5/weather?lat=',
 
    // forecast api
-   FORECAST_CITY: 'http://api.openweathermap.org/data/2.5/forecast?q=',
-   FORECAST_ZIP: 'http://api.openweathermap.org/data/2.5/forecast?zip='
+   FORECAST_CITY: 'https://api.openweathermap.org/data/2.5/forecast?q=',
+   FORECAST_ZIP: 'https://api.openweathermap.org/data/2.5/forecast?zip='
 
   //  http://api.openweathermap.org/data/2.5/forecast?q=atlanta&units=imperial&appid=691c1e153351f780c9a6e0268402a27b;
   // 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=691c1e153351f780c9a6e0268402a27b';
