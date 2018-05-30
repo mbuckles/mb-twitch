3239c67ea5cf9f2c58c1654f18fa26d5b647ffc5
mbuckles <bucklesm21@gmail.com>
2018-05-30 15:02:04 -0400

    heroku deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { WeatherComponent } from '../app/weather/weather.component';
 import { SearchComponent } from '../app/search/search.component';
 import { TwitchComponent } from '../app/twitch/twitch.component';
-// import { ServicesComponent } from '../app/services/weather.service';
-// import { ServicesComponent } from '../app/services/location.service';
 
 const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
-  // { path: 'weather', component: WeatherComponent },
-  // { path: 'search', component: SearchComponent },
   { path: 'twitch', component: TwitchComponent },
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
   // { path: '**', redirectTo: '/home', pathMatch: 'full' },
